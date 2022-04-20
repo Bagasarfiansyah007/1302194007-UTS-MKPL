6f2349281c8510f6d38cf08c81f70a0bfd2a0b4b
Bagasarfiansyah007 <bagasarfiansyah007@gmail.com>
2022-04-20 07:17:15 +0700

    Revision(Large class &  Long Parameter List - Bad Smell)
    Dalam commit ini saya merevisi penulisan code class saya menggunakan abstact dengan meng extend class salary, spouse dan year. Lalu merubah parameter menjadi lebih efisien lagi
    
    Daftar Perubahan :
    - Merubah class SALARY, SPOUSE, YEAR menggunakan abstact method yang meng extend ke class Employe
    
    - Mengubah parameter menjadi lebih sederhana lalu menggunakan class super untuk mengakses class parent

--- TaxCalculator/src/lib/Year.java
+++ TaxCalculator/src/lib/Year.java
@@ -1,15 +1,40 @@
 package lib;
 
-public class Year {
+import java.time.LocalDate;
+
+public class Year extends Employee{
     private int yearJoined;
     private int monthJoined;
     private int dayJoined;
     private int monthWorkingInYear;
 
-    public Year(int yearJoined, int monthJoined, int dayJoined, int monthWorkingInYear) {
+    public Year(Employee employee, int yearJoined, int monthJoined, int dayJoined)
+    {
+        super(
+                employee.getEmployeeId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getIdNumber(),
+                employee.getAddress(),
+                employee.isForeigner(),
+                employee.getGender()
+        );
         this.yearJoined = yearJoined;
         this.monthJoined = monthJoined;
         this.dayJoined = dayJoined;
-        this.monthWorkingInYear = monthWorkingInYear;
     }
+    public int getAnnualIncomeTax() {
+
+        //Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
+        LocalDate date = LocalDate.now();
+
+        if (date.getYear() == yearJoined) {
+            monthWorkingInYear = date.getMonthValue() - monthJoined;
+        }else {
+            monthWorkingInYear = 12;
+        }
+
+        return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+    }
+
 }
